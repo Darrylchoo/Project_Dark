bd15b425a463bc6a3885821e3213300b5fcdb80e
Darrylchoo <darrylchoo01@gmail.com>
2021-04-12 03:17:42 +0800

    Building light & screen shake effect

---
+++ Assets/Scripts/BuildingLight.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class BuildingLight : MonoBehaviour
+{
+    [SerializeField] private Light2D[] lights;
+
+    public float minInterval;
+    public float maxInterval;
+    public float flickerTimer;
+    public bool flicker;
+    private float rememberFlickerTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flicker = true;
+        StartCoroutine(Flicker());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Flicker()
+    {
+        while (flicker)
+        {
+            yield return new WaitForSeconds(flickerTimer);
+
+            int random = Random.Range(0, lights.Length);
+            lights[random].enabled = !lights[random].enabled;
+
+            rememberFlickerTimer += Time.deltaTime;
+
+            if (rememberFlickerTimer >= 1)
+            {
+                flicker = false;
+                rememberFlickerTimer = 0;
+
+                for (int i = 0; i < lights.Length; i++)
+                {
+                    lights[i].enabled = false;
+                }
+            }
+        }
+
+        if (!flicker)
+        {
+            yield return new WaitForSeconds(Random.Range(minInterval, maxInterval));
+            flicker = true;
+        }
+
+        StartCoroutine(Flicker());
+    }
+}
---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    public float shakeIntensity;
+    public float shakeDuration;
+    private Transform target;
+    private Vector3 initialPos;
+    private Vector3 randomPos;
+    private float shakeTimer;
+    private float startTime;
+    private bool isShaking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<Transform>();
+        initialPos = target.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakeTimer > 0 && !isShaking)
+        {
+            StartCoroutine(DoShake());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shake(shakeDuration);
+        }
+    }
+
+    public void Shake(float timer)
+    {
+        if (timer > 0)
+        {
+            shakeTimer += timer;
+        }
+    }
+
+    private IEnumerator DoShake()
+    {
+        isShaking = true;
+        startTime = Time.realtimeSinceStartup;
+
+        while (Time.realtimeSinceStartup < startTime + shakeTimer)
+        {
+            randomPos = new Vector3(Random.Range(-1, 1) * shakeIntensity, Random.Range(-1, 1) * shakeIntensity, initialPos.z);
+            target.localPosition = randomPos;
+            yield return null;
+        }
+
+        shakeTimer = 0;
+        target.localPosition = initialPos;
+        isShaking = false;
+    }
+}
